8badef3c0a33eb114d11b94889479be11f4214f8
Serafadam <serafadam@gmail.com>
2022-09-20 11:19:22 +0200

    changed pins

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,36 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 
-static const int RXPin = 1, TXPin = 3;
+static const int RXPin = 16, TXPin = 17;
 static const uint32_t GPSBaud = 9600;
-TinyGPSPlus gps; // The TinyGPS++ object
+TinyGPSPlus gps;                 // The TinyGPS++ object
 SoftwareSerial ss(RXPin, TXPin); // The serial connection to the GPS device
 
-void setup() {
-Serial.begin(115200);
-Serial.println("*****************************************************");
-
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("*****************************************************");
   ss.begin(GPSBaud);
 }
-void displayInfo() {
-  if (gps.location.isValid()) {
-double latitude = (gps.location.lat());
-double longitude = (gps.location.lng());
-Serial.println(latitude);
-Serial.println(longitude);
-}
-}
-void loop() {
-  while (ss.available() > 0){
-  if (gps.encode(ss.read())){
-  displayInfo();
-  }}
-}
+void displayInfo()
+{
 
+  if (gps.location.isValid())
+  {
+    double latitude = (gps.location.lat());
+    double longitude = (gps.location.lng());
+    Serial.println("***");
+    Serial.println(latitude, 6);
+    Serial.println(longitude, 6);
+  }
+}
+void loop()
+{
+  while (ss.available() > 0)
+  {
+    if (gps.encode(ss.read()))
+    {
+      displayInfo();
+    }
+  }
+}
